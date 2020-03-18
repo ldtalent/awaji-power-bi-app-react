c9f209e389764cf5a1b8c26d831d0f35c864483c
awaji-mitop GILBERT <successgilli@gmail.com>
2020-03-18 13:35:36 -0700

    feat(report): user can fetch report - fetch report from azure - install fetchjsonp - creat header component

--- app/components/app/index.jsx
+++ app/components/app/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Report from '../report/index.jsx';
+import Header from '../header/index.jsx';
 
 import './index.scss';
 
 const App = () => (
-    <Report/>
+    <div>
+        <Header />
+        <Report/>
+    </div>
 );
 
 export default App;
